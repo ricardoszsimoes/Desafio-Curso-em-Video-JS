06382d65d0d032e5cce0f27afaa113c8ce936cb8
Ricardo Simões <147839494+ricardoszsimoes@users.noreply.github.com>
2023-10-20 18:19:30 -0300

    desafios v1.0
    Desafios 1 e 2 adicionados.

---
+++ Desafio 001 JS/des001.js
@@ -0,0 +1,11 @@
+function clicou1() {
+    window.alert('Você clicou no 1º botão!')
+}
+
+function clicou2() {
+    window.alert('Você clicou no 2º botão!')
+}
+
+function clicou3() {
+    window.alert('Você clicou no 3º botão!')
+}
